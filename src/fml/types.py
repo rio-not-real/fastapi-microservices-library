bbc5afc3f0cbb8274fbe5ca79e15133b21332909
Rio Lin <riolin98@gmail.com>
2026-05-10 16:44:41 +1000

    feat: :sparkles: Add standard pagination/filtering/sorting models (#44)

--- src/fml/types.py
+++ src/fml/types.py
@@ -2,6 +2,12 @@
 
 from typing import Annotated
 
+from fastapi import Depends
 from fastapi.params import Header
 
+from fml.pagination import PaginationParams, SortParams
+
 RequestIdHeader = Annotated[str | None, Header(alias="X-Request-ID")]
+
+PaginationQuery = Annotated[PaginationParams, Depends()]
+SortQuery = Annotated[SortParams, Depends()]
